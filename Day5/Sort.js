17ee2837ac0376485cf3315643e1dce501c27885
Ani <anirudho747@gmail.com>
2024-10-11 13:25:10 +0530

    HCLTech

---
+++ Day5/Sort.js
@@ -0,0 +1,31 @@
+let a = [8,2,2,3,5,1,2,3,9,4,7,6];
+
+let temp;
+
+// for(let i = 0;i<a.length;i++)
+// {
+//     if(a[i]>a[i+1])
+//     {
+//         temp = a[i];
+//         a[i] = a[i+1];
+//         a[i+1] = temp;
+//     }
+// }
+
+for(let i = 0;i<a.length;i++)
+{
+    for(let j=i+1;j<a.length;j++)
+    {
+    if(a[i]>a[j])
+    {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
+}
+
+for(let i =0;i<a.length;i++)
+{
+        console.log(a[i]);
+}    
