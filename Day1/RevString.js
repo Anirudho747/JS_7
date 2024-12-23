d3faafde243da863385b38b396cb57bd4cbbbfbf
Anirudho747 <anirudho747@gmail.com>
2024-12-23 09:23:32 +0530

    Test Lead

--- Day1/RevString.js
+++ Day1/RevString.js
@@ -7,5 +7,4 @@ console.log(str.length);
 for(i=str.length-1;i>=0;i--)
 {
     process.stdout.write(str[i]);
-
 }
---
+++ Day7/Factorial.js
@@ -0,0 +1,17 @@
+const prompt  = require('prompt-sync')();
+
+let num  = parseInt(prompt("Enter number for which you want Factorial "));
+let factorial = 1;
+
+    if (isNaN(num) || num < 0)
+    {
+    console.log("Please enter a valid non-negative integer.");
+    }
+    else {
+
+        while (num > 1) {
+            factorial = factorial * num;
+            num--;
+        }
+    }
+console.log(factorial);
